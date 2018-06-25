72fa8cbc55cf3c1a268106c2ecd984489124917c
vu.phan <phannhpvu@gmail.com>
2018-06-25 14:17:03 +0700

    fixing errors at config js

--- view/adminhtml/requirejs-config.js
+++ view/adminhtml/requirejs-config.js
@@ -2,9 +2,9 @@ var config = {
     "map": {
         "*": {
             "myparcelcom_massaction": "MyParcelCOM_Magento/js/mass-action",
-            "myparcelcom_url_helper": "MyParcelCOM_Magento/js/mp-url-helper",
-            "myparcelcom_delivery_helper": "MyParcelCOM_Magento/js/mp-delivery-helper",
-            "myparcelcom_system_config": "MyParcelCOM_Magento/js/mp-system-config"
+            'myparcelcom_url_helper': 'MyParcelCOM_Magento/js/mp-url-helper',
+            'myparcelcom_delivery_helper': 'MyParcelCOM_Magento/js/mp-delivery-helper',
+            'myparcelcom_system_config': 'MyParcelCOM_Magento/js/mp-system-config'
         }
     }
 };
