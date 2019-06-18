4a9211110fe08c4b27543b15d6b77c2bf91e40b8
vu.phan <vu.phan@vn.devinition.com>
2019-06-18 15:48:32 +0700

    update configuration screen

--- Adapter/MpAdapter.php
+++ Adapter/MpAdapter.php
@@ -19,8 +19,10 @@ abstract class MpAdapter
     {
         $this->_objectManager   = ObjectManager::getInstance();
         $this->_configHelper    = $this->_objectManager->get('MyParcelCOM\Magento\Helper\MyParcelConfig');
-		$this->_authAPI         = 'https://staging-auth.myparcel.com'; // https://sandbox-auth.myparcel.com
-		$this->_urlAPI          = 'https://staging-api.myparcel.com'; // https://sandbox-api.myparcel.com
+		$this->_authAPI         = 'https://sandbox-auth.myparcel.com';
+		$this->_urlAPI          = 'https://sandbox-api.myparcel.com';
+		$this->_stage_authAPI   = 'https://staging-auth.myparcel.com';
+		$this->_stage_urlAPI    = 'https://staging-api.myparcel.com';
 
         $this->singletonApi();
     }
@@ -28,8 +30,14 @@ abstract class MpAdapter
     function singletonApi()
     {
         /**@var MyParcelConfig $this->_configHelper **/
+		$environment = $this->_objectManager->get('Magento\Framework\App\Config\ScopeConfigInterface')->getValue('myparcel_section_general/myparcel_group_api/api_client_environment');
+		$urlAPI = ($environment == 0) ? $this->_urlAPI : $this->_stage_urlAPI;
+		$authAPI = ($environment == 0) ? $this->_authAPI : $this->_stage_authAPI;
+		
+		// env
 		$urlAPI = getenv('MP_API_URL') ? : $this->_urlAPI;
 		$authAPI = getenv('MP_AUTH_URL') ? : $this->_authAPI;
+		
 
         // force token refresh (to get a token with new ACL scopes)
         $authenticator = new \MyParcelCom\ApiSdk\Authentication\ClientCredentials($this->_configHelper->getApiClientId(),  $this->_configHelper->getApiSecretKey(), $authAPI);
