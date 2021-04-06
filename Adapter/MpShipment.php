f471677304ff6ade2d5a821d744b83d756f55c4e
Martin Boer <martin@myparcel.com>
2021-04-06 02:06:25 +0200

    :fire: remove cron and print related code

--- Adapter/MpShipment.php
+++ Adapter/MpShipment.php
@@ -2,10 +2,10 @@
 
 namespace MyParcelCOM\Magento\Adapter;
 
-use MyParcelCom\ApiSdk\MyParcelComApi;
 use MyParcelCom\ApiSdk\Resources\Address;
 use MyParcelCom\ApiSdk\Resources\Customs;
 use MyParcelCom\ApiSdk\Resources\Interfaces\PhysicalPropertiesInterface;
+use MyParcelCom\ApiSdk\Resources\Interfaces\ShipmentInterface;
 use MyParcelCom\ApiSdk\Resources\PhysicalProperties;
 use MyParcelCom\ApiSdk\Resources\Shipment;
 use MyParcelCom\ApiSdk\Resources\ShipmentItem;
@@ -13,167 +13,78 @@ use MyParcelCom\ApiSdk\Resources\ShipmentItem;
 class MpShipment extends MpAdapter
 {
     private $_defaultAddressData = [
-        'street'       => '',
-        'house_number' => '',
-        'city'         => '',
-        'postcode'     => '',
         'first_name'   => '',
         'last_name'    => '',
+        'street'       => '',
+        'city'         => '',
+        'postcode'     => '',
         'country_code' => '',
         'email'        => '',
+        'phone_number' => '',
     ];
 
     private $_defaultShipmentData = [
-        'weight' => '0.1',
+        'weight' => 1000,
     ];
 
     /**
-     * Prepare the necessary data and create shipment
      * @param array  $addressData
      * @param array  $shipmentData
      * @param string $registerAt
-     * @return object response of the api
+     * @return ShipmentInterface
      **/
-    function createShipment($addressData, $shipmentData, $registerAt = '', $description = '', $items = '', $customs = '')
+    public function createShipment($addressData, $shipmentData, $registerAt = '', $description = null, $items = [], $customs = null)
     {
-        $api = MyParcelComApi::getSingleton();
-
-        $mpCarrier = new MpCarrier();
         $mpShop = new MpShop();
+        $shop = $mpShop->getDefaultShop();
 
         $addressData = array_merge($this->_defaultAddressData, $addressData);
         $shipmentData = array_merge($this->_defaultShipmentData, $shipmentData);
 
-        $recipient = new Address();
+        $physicalProperties = (new PhysicalProperties())
+            ->setWeight($shipmentData['weight'], PhysicalPropertiesInterface::WEIGHT_GRAM);
 
-        $recipient
+        $recipient = (new Address())
+            ->setFirstName($addressData['first_name'])
+            ->setLastName($addressData['last_name'])
             ->setStreet1($addressData['street'])
             ->setCity($addressData['city'])
             ->setPostalCode($addressData['postcode'])
-            ->setFirstName($addressData['first_name'])
-            ->setLastName($addressData['last_name'])
             ->setCountryCode($addressData['country_code'])
             ->setEmail($addressData['email'])
             ->setPhoneNumber($addressData['phone_number']);
 
-        $shipment = new Shipment();
+        $shipment = (new Shipment())
+            ->setShop($shop)
+            ->setRecipientAddress($recipient)
+            ->setSenderAddress($shop->getSenderAddress())
+            ->setReturnAddress($shop->getReturnAddress())
+            ->setPhysicalProperties($physicalProperties)
+            ->setDescription($description);
 
-        $shipment->setRecipientAddress($recipient);
+        foreach ($items as $item) {
+            $shipmentItem = (new ShipmentItem())
+                ->setSku($item['sku'])
+                ->setDescription($item['description'])
+                ->setQuantity($item['quantity'])
+                ->setHsCode($item['hs_code'])
+                ->setOriginCountryCode($item['origin_country_code'])
+                ->setCurrency($item['item_value']['currency'])
+                ->setItemValue($item['item_value']['amount']);
 
-        if (!empty($shipmentData['weight'])) {
-            $physicalProperties = (new PhysicalProperties())
-                ->setWeight($shipmentData['weight'], PhysicalPropertiesInterface::WEIGHT_GRAM);
-            $shipment->setPhysicalProperties($physicalProperties);
+            $shipment->addItem($shipmentItem);
         }
 
-        /**
-         * SET SENDER ADDRESS
-         * In some cases, sender address is required
-         **/
-        $shop = $mpShop->getDefaultShop();
-        $senderAddress = $shop->getSenderAddress();
-        $shipment->setSenderAddress($senderAddress);
-
-        /**
-         * SET RETURN ADDRESS
-         * In some cases, return  address is required
-         **/
-        $returnAddress = $shop->getReturnAddress();
-        $shipment->setReturnAddress($returnAddress);
-
-        /**
-         * Set Register At value for shipment
-         **/
-        if (!empty($registerAt)) {
-            $shipment->setRegisterAt($registerAt);
-        }
-
-        /**
-         * set relationship shop for shipment
-         */
-        $shipment->setShop($shop);
-
-        /**
-         * Set Description At value for shipment
-         **/
-        if (!empty($description)) {
-            $shipment->setDescription($description);
-        }
-
-        /**
-         * Set Items value for shipment.
-         */
-        if (!empty($items)) {
-            $shipmentItems = [];
-            foreach ($items as $item) {
-                $shipmentItem = new ShipmentItem();
-                $shipmentItem->setSku($item['sku']);
-                $shipmentItem->setDescription($item['description']);
-                $shipmentItem->setQuantity($item['quantity']);
-                $shipmentItem->setHsCode($item['hs_code']);
-                $shipmentItem->setOriginCountryCode($item['origin_country_code']);
-                $shipmentItem->setCurrency($item['item_value']['currency']);
-                $shipmentItem->setItemValue($item['item_value']['amount']);
-
-                $shipmentItems[] = $shipmentItem;
-            }
-            $shipment->setItems($shipmentItems);
-        }
-
-        /**
-         * Set customs value for shipment
-         */
         if (!empty($customs)) {
-            $shipmentCustoms = new Customs();
-            $shipmentCustoms->setContentType($customs['content_type']);
-            $shipmentCustoms->setInvoiceNumber($customs['invoice_number']);
-            $shipmentCustoms->setIncoterm($customs['incoterm']);
-            $shipmentCustoms->setNonDelivery($customs['non_delivery']);
+            $shipmentCustoms = (new Customs())
+                ->setContentType($customs['content_type'])
+                ->setInvoiceNumber($customs['invoice_number'])
+                ->setIncoterm($customs['incoterm'])
+                ->setNonDelivery($customs['non_delivery']);
 
             $shipment->setCustoms($shipmentCustoms);
         }
 
-        $response = $api->createShipment($shipment);
-
-        return $response;
-    }
-
-    function getShipment($shipmentId)
-    {
-        $api = MyParcelComApi::getSingleton();
-        $shipment = $api->getShipment($shipmentId);
-
-        return $shipment;
-    }
-
-    function getFiles($shipmentId)
-    {
-        $api = MyParcelComApi::getSingleton();
-        $shipment = $api->getShipment($shipmentId);
-        $files = $shipment->getFiles();
-
-        return $files;
-    }
-
-    function getStatus($shipmentId)
-    {
-        // Get the current status of the shipment.
-        $api = MyParcelComApi::getSingleton();
-        $shipment = $api->getShipment($shipmentId);
-        $status = $shipment->getStatus();
-
-        return $status;
-    }
-
-    /**
-     * @param Shipment $shipment
-     * @param string   $when
-     * @return mixed
-     **/
-    function setRegisterAt($shipment, $when = 'now')
-    {
-        $api = MyParcelComApi::getSingleton();
-        $shipment->setRegisterAt($when);
-        return $api->updateShipment($shipment);
+        return $this->getApi()->createShipment($shipment);
     }
 }
